6041453c1a084ceae8a0b83d25c812c4b7781cf6
sayaka-38 <yaki_nigi_uma@yahoo.co.jp>
2020-04-20 09:53:38 +0900

    Add tag table

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
+
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create]
+  resources :tags, only: [:new, :create]
+
 end
