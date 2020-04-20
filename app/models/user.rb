10f4f647a23f024fa21774694db8f137183dc9f0
sayaka-38 <yaki_nigi_uma@yahoo.co.jp>
2020-04-20 16:43:55 +0900

    Add Post model-controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tags, dependent: :destroy
+  has_many :posts, dependent: :destroy
          
   validates :name, presence: true, uniqueness: true, length: { maximum: 15 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_133648) do
+ActiveRecord::Schema.define(version: 2020_04_20_073001) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.string "image"
+    t.bigint "tag_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tag_id"], name: "index_posts_on_tag_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -34,5 +45,7 @@ ActiveRecord::Schema.define(version: 2020_04_19_133648) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "tags"
+  add_foreign_key "posts", "users"
   add_foreign_key "tags", "users"
 end
