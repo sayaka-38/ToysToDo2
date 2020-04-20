a5f1bea6371c0745cdc8f421fe6b7c19f9b12a01
sayaka-38 <yaki_nigi_uma@yahoo.co.jp>
2020-04-20 12:54:03 +0900

    Add tag-new-create

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,17 @@
 class TagsController < ApplicationController
+
   def new
+    @tag = Tag.new
   end
 
   def create
+    Tag.create(tag_params)
+    redirect_to root_path
   end
+
+  private
+  def tag_params
+    params.require(:tag).permit(:title).merge(user_id: current_user.id)
+  end
+
 end
---
+++ app/models/tag.rb
@@ -0,0 +1,5 @@
+class Tag < ApplicationRecord
+  belongs_to :user
+  
+  validates :title, length: { in: 1..255 }, presence: true
+end
