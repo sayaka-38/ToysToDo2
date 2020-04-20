458febc59902e2547cc191850c5c5b77e46fc461
sayaka-38 <yaki_nigi_uma@yahoo.co.jp>
2020-04-20 14:44:00 +0900

    Add tag-edit-create

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -9,6 +9,16 @@ class TagsController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @tag = Tag.find(params[:id])
+  end
+
+  def update
+    tag = Tag.find(params[:id])
+    tag.update(tag_params)
+    redirect_to root_path
+  end
+
   private
   def tag_params
     params.require(:tag).permit(:title).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   root 'top#index'
 
   resources :users, only: [:edit, :update]
-  resources :tags, only: [:new, :create]
-
+  resources :tags, only: [:new, :create, :edit, :update]
 end
