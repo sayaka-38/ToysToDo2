8d9fe8da0e833d369a97dc19f17edd77f0ff84f7
sayaka-38 <yaki_nigi_uma@yahoo.co.jp>
2020-05-18 13:09:40 +0900

    Edit posts.order

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,7 +2,7 @@ class TopController < ApplicationController
 
   def index
     @tags = Tag.all
-    @posts = Post.includes(:user)
+    @posts = Post.includes(:user).order("id DESC")
     @post = Post.new
   end
 
